e3ab660fdfb4ec9a628b872ab1ad75506fe6c0a8
MaxySpark <maxyspark@gmail.com>
2017-02-10 21:43:08 +0530

    added total cgpa

--- js/index.js
+++ js/index.js
@@ -15,21 +15,40 @@ function addInputRow() {
     }
 }
 
+var total_credit = 0;
+var total_sgpa_credit = 0;
+
 function cgpaCalculate(){
-    var sgpa = document.getElementsByClassName("sgpa_in")[0].value;
-    if(sgpa=="") {
-        sgpa = 0;
-    }
-    var credit = document.getElementsByClassName("credit_in")[0].value;
-    if(credit=="") {
-        credit = 0;
-    }
-    var cgpa = ((sgpa * credit) / credit);
-    if(isNaN(cgpa)) cgpa = 0;
+    var sgpa_arr = document.getElementsByClassName("sgpa_in");
+    var credit_arr = document.getElementsByClassName("credit_in");
+    elem_count = 0;
+    
+    while(elem_count < sgpa_arr.length) {
+        var sgpa = sgpa_arr[elem_count].value;
+        if(sgpa=="") {
+            sgpa = 0;
+        } else {
+            sgpa = parseFloat(sgpa);
+        }
+        var credit = credit_arr[elem_count].value;
+        if(credit=="") {
+            credit = 0;
+        } else {
+            credit = parseFloat(credit);
+        }
+        var cgpa = ((sgpa * credit) / credit);
+        total_credit += credit;
+        total_sgpa_credit += (sgpa * credit);
+        if(isNaN(cgpa)) cgpa = 0;
 
-    document.getElementsByClassName("cgpa_out")[0].value = cgpa;
+        document.getElementsByClassName("cgpa_out")[elem_count].value = cgpa;
+        elem_count++;
 
-    console.log("SGPA : "+ sgpa);
-    console.log("CREDIT : "+ credit);
-    console.log("CGPA : "+ cgpa);  
+        console.log("\nSGPA : "+ sgpa);
+        console.log("CREDIT : "+ credit);
+        console.log("CGPA : "+ cgpa);
+    }
+
+    var total_cgpa = (total_sgpa_credit / total_credit);
+    document.getElementById("total_cgpa").innerHTML = total_cgpa;  
 }
